4bd5915a18912d1d12d4ed9dc8687249da350196
Derv6464 <dervlag6464@gmail.com>
2024-11-18 10:06:14 +0000

    adding flask things

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from helpers.logger import Logger_Helper
 from helpers.config import Config_Helper
 
+from flask import Flask, request, jsonify
+
 config = Config_Helper('config.json')
 
 print(config.get('logs_location'))
@@ -8,3 +10,15 @@ Logger_Helper.setUp(config.get('logs_location'))
 logger = Logger_Helper.logger
 
 logger.info('This is an info message')
+
+app = Flask(__name__)
+@app.route('/hello')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/data')
+def data():
+    return "metrics"
+
+if __name__ == '__main__':
+    app.run()
